bf33829ef3a4557ae285a29d81c541e6372bd793
T0mLam <lamlamlam010406@gmail.com>
2024-08-20 18:39:28 +0100

    update loss

--- res_unet_a/loss.py
+++ res_unet_a/loss.py
@@ -1,6 +1,6 @@
 import torch 
 from torch import nn
-import torch.functional as F
+import torch.nn.functional as F
 
 
 class TanimotoLoss(nn.Module):
@@ -13,11 +13,15 @@ class TanimotoLoss(nn.Module):
         y_pred: (b, c, h, w)
         y: (b, c, h, w)
         """
-        p, l = F.softmax(y_pred, 1), y
+        p, l = y_pred, y
         epsilon = 1e-7
 
-        V = torch.mean(torch.sum(l, dim=[2, 3]), dim=0)
+        V = torch.mean(torch.sum(l, dim=[2, 3], dtype=torch.float), dim=0)
         w = V ** -2
+
+        inf = torch.tensor(float('inf'))
+        new_weights = torch.where(w == inf, torch.zeros_like(w), w)
+        w = torch.where(w == inf, torch.ones_like(w) * torch.max(new_weights), w)
 
         p2 = p ** 2
         l2 = l ** 2
@@ -31,7 +35,7 @@ class TanimotoLoss(nn.Module):
 
         loss = numerator / (denominator + epsilon)
 
-        return loss
+        return 1 - loss
     
 
 class ComplementedTanimotoLoss(nn.Module):
@@ -39,6 +43,7 @@ class ComplementedTanimotoLoss(nn.Module):
         self.loss_func = TanimotoLoss()
 
     def forward(self, y_pred, y):
+        y_pred = F.softmax(y_pred, 1)
         loss = self.loss_func(y_pred, y)
         loss_complement = self.loss_func(1 - y_pred, 1 - y)
 
