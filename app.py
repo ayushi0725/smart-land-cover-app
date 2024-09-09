726a6765b83f284a343c6d01a90a368d7a2751f4
T0mLam <lamlamlam010406@gmail.com>
2024-09-09 19:14:03 +0100

    update app

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from PIL import Image
 import streamlit as st
 import torch
 from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
 
 from dataset import SatelliteImageDataset
 from res_unet_a import ResUNetA
@@ -82,14 +83,28 @@ def predict(x):
 
         model = unet if model_selection == 'UNet' else res_unet_a
         y_pred = model(torch.unsqueeze(x, 0).to(DEVICE))
-
         pred_img = one_hot_to_image(y_pred, COLOR_MAP, DEVICE)
 
     return pred_img
 
 
-def preprocessing(img):
-    pass
+def preprocessing(im):
+    width, height = im.size
+    target = min(width, height)
+    
+    left = (width - target) / 2
+    top = (height - target) / 2
+    right = (width + target) / 2
+    bottom = (height + target) / 2
+
+    croped_img = im.crop((left, top, right, bottom))
+    resized_img = transforms.Resize((512, 512))(croped_img)
+    saturated_img = transforms.ColorJitter(contrast=(1.25, 1.25))(resized_img)
+    sharpened_img = transforms.functional.adjust_sharpness(saturated_img, 2)
+    
+    img_tensor = transforms.ToTensor()(sharpened_img)
+    
+    return sharpened_img, img_tensor
 
 
 st.header("""
@@ -98,6 +113,8 @@ developed by Tom Lam
 """)
 
 img_col1, img_col2 = st.columns(2)
+img_preview = st.empty()
+img_prediction = st.empty()
 img_class_placeholder = st.empty()
 
 with st.sidebar:
@@ -119,5 +136,14 @@ if example_button:
     gen_example()
 
 if custom_image_button and uploaded_file:
-    image = np.asarray(Image.open(uploaded_file), dtype=np.int32)
-    img = preprocessing(image)
+    im = Image.open(uploaded_file).convert("RGB")
+    img, img_tensor = preprocessing(im)
+
+    image = np.asarray(img)
+    img_preview.image(image, caption="Preview")
+
+    x = img_tensor
+    print(x.shape)
+    print(x.min(), x.max())
+    pred_img = torch.squeeze(predict(x), 0)
+    img_prediction.image(pred_img.cpu().permute(1, 2, 0).numpy(), caption="Prediction")
